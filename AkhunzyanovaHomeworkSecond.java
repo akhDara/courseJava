4b31bd5623251db51a0234c79d6dfddf13667187
Guldar <ahunzanovaguldar@gmail.com>
2022-03-31 06:17:28 +0300

    Исправленное дз #2 и ДЗ #3

--- AkhunzyanovaHomeworkSecond.java
+++ AkhunzyanovaHomeworkSecond.java
@@ -1,14 +1,14 @@
 /**
 * Java 1. Homework 2
 * @author Guldar Akhunzyanova
-* @version 29.03.2021
+* @version #2 29.03.2021
 */
 class AkhunzyanovaHomeworkSecond {
     public static void main(String[] args) {
         System.out.println(compareSumOfNumbers(10, 5));
         System.out.println(printPositivOrNegativeNumber(0));
         System.out.println(comparePositivOrNegativeNumber(-133));
-        printLoops(5);
+        printLoops(" пингвин(а)", 5);
         System.out.println(checkingTheLeapYear(1996));
         }
     static boolean compareSumOfNumbers(int a, int b) {
@@ -20,9 +20,9 @@ class AkhunzyanovaHomeworkSecond {
     static boolean comparePositivOrNegativeNumber (int a) {
         return a >=0? true : false;
     }
-    static void printLoops(int a) {
+    static void printLoops(String s, int a) {
         for (int i = 1; i <= 5; i++) {
-            System.out.println(i + " строка");
+            System.out.println(s);
         }
     }
     static boolean checkingTheLeapYear(int a) {
---
+++ AkhunzyanovaHomeworkThird.java
@@ -0,0 +1,59 @@
+/**
+* Java 1. Homework 2
+* @author Guldar Akhunzyanova
+* @version #1 29.03.2021
+*/
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class AkhunzyanovaHomeworkThird {
+    public static void main(String args[]) {
+        print1DArray();
+        fillRow();
+        multiplyNumbersUnderSix();
+        fillDiagonals();
+        fillScanner();
+    }
+    static void print1DArray() {
+        int[] arr = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] == 1?  0 : 1);
+        }
+        System.out.println();
+    }
+    static void fillRow() {
+        int[] arr = new int[100];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    static void multiplyNumbersUnderSix() {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] < 6? arr[i] * 2 + " " : arr[i] + " ");
+        }
+        System.out.println();
+    }
+    static void fillDiagonals() {
+        int[][] arr = new int[5][5];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(i == j || arr.length - i - 1 == j? 1 : 0);
+            }
+            System.out.println();
+        }
+    }
+    static void fillScanner() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter len and initialValue: ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int[] arr = new int[a];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] = b);
+        }
+    }
+}
