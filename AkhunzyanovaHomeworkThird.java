c55ef54c2c5a8f8d63ef42a82bef7bb608774aba
Guldar <ahunzanovaguldar@gmail.com>
2022-04-04 01:42:27 +0300

    Исправленное дз (но без использования Random)

--- AkhunzyanovaHomeworkThird.java
+++ AkhunzyanovaHomeworkThird.java
@@ -1,58 +1,56 @@
 /**
-* Java 1. Homework 2
-* @author Guldar Akhunzyanova
-* @version #1 29.03.2021
-*/
+ * Java 1. Homework 2
+ * @author Guldar Akhunzyanova
+ * @version #2 29.03.2021
+ */
 
-import java.util.Scanner;
+import java.util.Arrays;
 
-class AkhunzyanovaHomeworkThird {
-    public static void main(String args[]) {
-        print1DArray();
-        fillRow();
-        multiplyNumbersUnderSix();
-        fillDiagonals();
-        fillScanner();
+
+public class AkhunzyanovaHomeworkThird {
+    public static void main(String[] args) {
+        fillArray(new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0});
+        print1DArray(new int[100]);
+        multiplyNumbersUnderSix(new int[] {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1});
+        fillDiagonals(new int[5][5]);
+        createArr(5, 9);
+
+
     }
-    static void print1DArray() {
-        int[] arr = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] == 1?  0 : 1);
+    static void fillArray(int[] arr) {
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = 1 - arr[i];
+            }
+            System.out.println(Arrays.toString(arr));
         }
-        System.out.println();
-    }
-    static void fillRow() {
-        int[] arr = new int[100];
+    static void print1DArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i + 1;
-            System.out.print(arr[i] + " ");
         }
-        System.out.println();
+        System.out.println(Arrays.toString(arr));
     }
-    static void multiplyNumbersUnderSix() {
-        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+    static void multiplyNumbersUnderSix(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] < 6? arr[i] * 2 + " " : arr[i] + " ");
-        }
-        System.out.println();
-    }
-    static void fillDiagonals() {
-        int[][] arr = new int[5][5];
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                System.out.print(i == j || arr.length - i - 1 == j? 1 : 0);
+            if (arr[i] < 6) {
+                arr[i] *= 2;
             }
-            System.out.println();
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    static void fillDiagonals(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i][i] = 1;
+            arr[i][arr.length - i - 1] = 1;
+            }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(Arrays.toString(arr[i]));
         }
     }
-    static void fillScanner() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter len and initialValue: ");
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int[] arr = new int[a];
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] = b);
+    static void createArr(int len, int initialValue) {
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = initialValue;
         }
+        System.out.println(Arrays.toString(arr));
     }
 }
